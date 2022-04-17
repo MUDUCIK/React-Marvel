bb699729a0b89bfb32471d69a71fb83ac4adfaf7
Renat <twil.revaas@gmail.com>
2022-04-17 23:17:31 +0300

    added error route

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
                 </>
               }
             />
+            <Route path={'*'} component={<div>Page not found</div>} />
           </Routes>
         </div>
       </main>
